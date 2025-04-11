d7e76ce9e7d6988115d2df2ea77e419f04ccaa32
Laura jimenez <emmabluebeachwaer@gmail.com>
2025-04-11 18:48:13 -0500

    update of lading page

--- mi-app/src/components/heartCard/heartCard.tsx
+++ mi-app/src/components/heartCard/heartCard.tsx
@@ -1,5 +1,12 @@
-// components/BannerCard.tsx
+
 'use client';
+import { Edu_VIC_WA_NT_Beginner } from 'next/font/google';
+
+const eduFont = Edu_VIC_WA_NT_Beginner({
+  subsets: ['latin'],
+  weight: '400', // también podés usar 500 o 700 si querés más grosor
+});
+
 
 import { useEffect, useState } from 'react';
 
@@ -15,36 +22,27 @@ export default function BannerCard() {
   }, []);
 
   return (
-    <div className="relative w-full h-72 bg-orange-400 rounded-xl shadow-xl overflow-hidden flex items-center justify-center ">
-      {/* Imagen o ícono */}
-      <img
-        src="/cama.png"
-        alt="Perrito"
-        className="w-200 h-200 "
-      />
-      <section className="h-96 bg-orange-400 flex items-center justify-between px-10">
-  {/* Texto de bienvenida al lado izquierdo */}
-  <div>
-    <h1 className="text-8xl font-bold text-white">LOVE</h1>
-    <p className="text-white mt-4 text-lg">
-    "La cama de tus sueños… ¡ahora para tu peludo!
-Diseñada con materiales ultra suaves, soporte ortopédico y estilo único, nuestra cama ChillPets es el lugar perfecto para que tu mascota descanse como se merece.
-¡Porque su comodidad también es amor!
-    </p>
-  </div>
+  
+<div className="relative w-full bg-orange-400 rounded-xl shadow-xl flex items-center justify-center px-4 py-8">
+  <div className="flex flex-col items-center w-full max-w-3xl">
+    
+    {/* Imagen 1 */}
+    <img
+      src="/amor3.png"
+      alt="Amor"
+      className="w-full max-w-xl object-contain mb-0"
+    />
 
-  {/* Botón y texto al lado derecho */}
-  <div className="flex flex-col items-end">
-    <p className="text-white text-lg mb-2"></p>
-    <button className="bg-yellow-400 text-white font-semibold py-3 px-6 rounded hover:bg-yellow-500 transition duration-300">
+    
+
+    {/* Botón */}
+    <button className="mt-2 bg-yellow-400 text-white font-semibold py-2 px-6 rounded hover:bg-yellow-500 transition duration-300">
       Comprar
     </button>
   </div>
-</section>
+</div>
 
 
-      
 
-    </div>
   );
 }
